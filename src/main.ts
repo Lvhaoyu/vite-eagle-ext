5ac9dfd8142c1ad258cf064fdd8206e9b80d2e83
Logan Lyu <lhy472075257@gmail.com>
2023-11-10 12:29:33 +0800

    delete: 删除多余文件

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { useGlobalData } from './hooks/useGlobalData'
-
-useGlobalData()
 
 eagle.onPluginCreate(() => {
   createApp(App).mount('#app')
